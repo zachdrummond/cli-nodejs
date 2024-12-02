04e36e3571493ed6712f1ec93ac54b6cee2eec70
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2024-12-02 17:26:41 -0500

    Created switch case for commands

--- index.js
+++ index.js
@@ -2,5 +2,21 @@ import process from "node:process";
 
 if(process.argv.length > 2){
     const commands = process.argv.slice(2);
-    console.log(commands);
+
+    switch(commands[0]){
+        case 'add':
+            console.log('add');
+            break;
+        case 'update':
+            console.log('update');
+            break;
+        case 'delete':
+            console.log('delete');
+            break;
+        case 'list':
+            console.log('list');
+            break;
+        default:
+            console.log('default');
+    }
 }
