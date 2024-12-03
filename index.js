5840e3a504a1faecdd1ea90e3632518468ff94b4
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2024-12-03 09:38:13 -0500

    Created todo-list.json

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+import * as fs from 'node:fs/promises';
 import process from "node:process";
 
 if(process.argv.length > 2){
     const commands = process.argv.slice(2);
+    const command = commands[0];
 
-    switch(commands[0]){
+    switch(command){
         case 'add':
-            console.log('add');
+            const task = commands[1];
+            update_todo_list(task);
             break;
         case 'update':
             console.log('update');
@@ -18,5 +21,14 @@ if(process.argv.length > 2){
             break;
         default:
             console.log('default');
+    }
+}
+
+async function update_todo_list(data){
+    try {
+        await fs.writeFile('todo-list.json', data);
+        console.log(data);
+    } catch (err) {
+        console.log(err);
     }
 }
