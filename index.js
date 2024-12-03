50bef04c49bdee0401767bf3c27ccb65e147098c
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2024-12-03 17:04:09 -0500

    Works with json

--- index.js
+++ index.js
@@ -1,49 +1,21 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("node:fs/promises");
-var node_process_1 = require("node:process");
-if (node_process_1.default.argv.length > 2) {
-    var commands = node_process_1.default.argv.slice(2);
-    var command = commands[0];
-    switch (command) {
+import * as fs from 'node:fs/promises';
+import * as process from "node:process";
+
+// type todo = {
+//     id: number,
+//     description: string,
+//     status: "todo"| "in-progress"| "done",
+//     createdAt: Date,
+//     updatedAt: Date
+// };
+
+if(process.argv.length > 2){
+    const commands = process.argv.slice(2);
+    const command = commands[0];
+
+    switch(command){
         case 'add':
-            var description = commands[1];
+            const description = commands[1];
             update_todo_list(description);
             break;
         case 'update':
@@ -59,33 +31,21 @@ if (node_process_1.default.argv.length > 2) {
             console.log('default');
     }
 }
-function update_todo_list(description) {
-    return __awaiter(this, void 0, void 0, function () {
-        var todo, err_1;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    todo = {
-                        id: 1,
-                        description: description,
-                        status: "todo",
-                        createdAt: new Date(),
-                        updatedAt: new Date()
-                    };
-                    _a.label = 1;
-                case 1:
-                    _a.trys.push([1, 3, , 4]);
-                    return [4 /*yield*/, fs.writeFile('todo-list.json', todo)];
-                case 2:
-                    _a.sent();
-                    console.log(todo);
-                    return [3 /*break*/, 4];
-                case 3:
-                    err_1 = _a.sent();
-                    console.log(err_1);
-                    return [3 /*break*/, 4];
-                case 4: return [2 /*return*/];
-            }
-        });
-    });
+
+async function update_todo_list(description){
+
+    const todo = {
+        id: 1,
+        description: description,
+        status: "todo",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }
+
+    try {
+        await fs.writeFile('todo-list.json', JSON.stringify(todo));
+        console.log(todo);
+    } catch (err) {
+        console.log(err);
+    }
 }
