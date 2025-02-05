14f17d3ce2434b69b1aca75b6506e8e91b462b96
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-02-05 09:26:25 -0500

    Updated author

--- packages/cli-commander/src/index.ts
+++ packages/cli-commander/src/index.ts
@@ -1,27 +1,27 @@
 import * as process from "node:process";
-import {taskhub} from "@cli-nodejs/taskhub"
-import {package_boilerplate} from "@cli-nodejs/pkg-bp";
+import { taskhub } from "@cli-nodejs/taskhub";
+import { package_boilerplate } from "@cli-nodejs/pkg-bp";
 
-if (process.argv.length > 2) {
-  const commands = process.argv.slice(2);
+const commands = process.argv.slice(2);
 
-  switch (commands[0]) {
-    case "taskhub":
-      taskhub(commands);
-      break;
-    case "boilerplate":
-    case "pkg-bp":
-      package_boilerplate(commands);
-      break;
-    case "help":
-    default:
-      console.log(
-        "CLI Commander - A command line interface application used to command all packages within cli-nodejs."
-      );
-      console.log("\nOptions:");
-      console.log("taskhub       Track and manage your tasks.");
-      console.log("boilerplate   Create the boilerplate files for a new package.");
-      console.log("help          Show the list of command options.");
-      break;
-  }
+switch (commands[0]) {
+  case "taskhub":
+    taskhub(commands);
+    break;
+  case "boilerplate":
+  case "pkg-bp":
+    package_boilerplate(commands);
+    break;
+  case "help":
+  default:
+    console.log(
+      "CLI Commander - A command line interface application used to command all packages within cli-nodejs."
+    );
+    console.log("\nOptions:");
+    console.log("taskhub       Track and manage your tasks.");
+    console.log(
+      "boilerplate   Create the boilerplate files for a new package."
+    );
+    console.log("help          Show the list of command options.");
+    break;
 }
