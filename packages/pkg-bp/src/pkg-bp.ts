7c097d974d916b84226c208ef56881119e8b63ad
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-03-07 11:34:29 -0500

    Updated pkg-bp

--- packages/pkg-bp/src/pkg-bp.ts
+++ packages/pkg-bp/src/pkg-bp.ts
@@ -1,8 +1,13 @@
 import { execSync } from "child_process";
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
 
-export default function package_boilerplate(commands: string[]) {
+interface PackageOptions {
+  scope?: string;
+  description?: string;
+}
+
+export default async function package_boilerplate(commands: string[], options: PackageOptions = { scope: "@cli-nodejs" }): Promise<void> {
   const packageName = commands[1];
 
   if (!packageName) {
@@ -12,12 +17,16 @@ export default function package_boilerplate(commands: string[]) {
 
   const packagePath = path.join("packages", packageName);
 
-  // Step 1: Initialize the new package
   try {
-    execSync(`npm init -w ./${packagePath} --scope=@cli-nodejs -y`, { stdio: "inherit" });
-    console.log(`Package "${packageName}" created successfully.`);
+    // Step 1: Ensure directory exists
+    await fs.mkdir(packagePath, { recursive: true });
 
-    // Step 2: Create tsconfig.json
+    // Step 2: Initialize the new package
+    const scope = options.scope ? `--scope=${options.scope}` : "";
+    const description = options.description ? `--description="${options.description}"` : "";
+    execSync(`npm init -w ./${packagePath} ${scope} ${description} -y`, { stdio: "inherit", encoding: "utf-8" });
+
+    // Step 3: Create tsconfig.json
     const tsconfig = {
       extends: "../../tsconfig.json",
       compilerOptions: {
@@ -29,19 +38,36 @@ export default function package_boilerplate(commands: string[]) {
       exclude: ["dist"],
     };
 
-    fs.writeFileSync(
+    await fs.writeFile(
       path.join(packagePath, "tsconfig.json"),
       JSON.stringify(tsconfig, null, 2)
     );
 
     // Step 3: Create README.md
-    const readmeContent = `# ${packageName}`;
-    fs.writeFileSync(path.join(packagePath, "README.md"), readmeContent);
+    const readmeContent = `# ${packageName}
 
-    // Step 4: Create src/index.js
+    ## Description
+    <Insert description here>
+
+    ## Features
+    - <Insert features here>
+
+    ## Usage
+    - <Insert usage here>
+
+    ## Tools
+    - [Node.js](https://nodejs.org/en)
+    - [TypeScript](https://www.typescriptlang.org/)
+    `;
+    fs.writeFile(path.join(packagePath, "README.md"), readmeContent);
+
+    // Step 5: Create src/index.ts
     const srcDir = path.join(packagePath, "src");
-    fs.mkdirSync(srcDir, { recursive: true });
-    fs.writeFileSync(path.join(srcDir, `${packageName}.js`), "");
+    await fs.mkdir(srcDir, { recursive: true });
+    await fs.writeFile(path.join(srcDir, `${packageName}.ts`), `
+    export default function ${packageName}() {
+      console.log("Hello from ${packageName}!");
+    }`);
 
     console.log(`Package "${packageName}" created successfully.`);
   } catch (error: any) {
