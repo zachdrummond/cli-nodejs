1072b9cb6165b999a2a52e7bc6b641bbcd20265d
Zach Drummond <68283144+zachdrummond@users.noreply.github.com>
2025-03-10 13:56:02 -0400

    Updated boilerplate

--- packages/pkg-bp/src/pkg-bp.ts
+++ packages/pkg-bp/src/pkg-bp.ts
@@ -2,12 +2,9 @@ import { execSync } from "child_process";
 import fs from "fs/promises";
 import path from "path";
 
-interface PackageOptions {
-  scope?: string;
-  description?: string;
-}
-
-export default async function package_boilerplate(commands: string[], options: PackageOptions = { scope: "@cli-nodejs" }): Promise<void> {
+export default async function package_boilerplate(
+  commands: string[]
+): Promise<void> {
   const packageName = commands[1];
 
   if (!packageName) {
@@ -22,9 +19,10 @@ export default async function package_boilerplate(commands: string[], options: P
     await fs.mkdir(packagePath, { recursive: true });
 
     // Step 2: Initialize the new package
-    const scope = options.scope ? `--scope=${options.scope}` : "";
-    const description = options.description ? `--description="${options.description}"` : "";
-    execSync(`npm init -w ./${packagePath} ${scope} ${description} -y`, { stdio: "inherit", encoding: "utf-8" });
+    execSync(
+      `npm init -w ./${packagePath} --scope=@cli-nodejs -y`,
+      { stdio: "inherit", encoding: "utf-8" }
+    );
 
     // Step 3: Create tsconfig.json
     const tsconfig = {
@@ -44,30 +42,34 @@ export default async function package_boilerplate(commands: string[], options: P
     );
 
     // Step 3: Create README.md
-    const readmeContent = `# ${packageName}
+    const readmeContent = `
+# ${packageName}
 
-    ## Description
-    <Insert description here>
+## Description
+<Insert description here>
 
-    ## Features
-    - <Insert features here>
+## Features
+- <Insert features here>
 
-    ## Usage
-    - <Insert usage here>
+## Usage
+- <Insert usage here>
 
-    ## Tools
-    - [Node.js](https://nodejs.org/en)
-    - [TypeScript](https://www.typescriptlang.org/)
-    `;
-    fs.writeFile(path.join(packagePath, "README.md"), readmeContent);
+## Tools
+- [Node.js](https://nodejs.org/en)
+- [TypeScript](https://www.typescriptlang.org/)`;
+
+    await fs.writeFile(path.join(packagePath, "README.md"), readmeContent);
 
     // Step 5: Create src/index.ts
     const srcDir = path.join(packagePath, "src");
     await fs.mkdir(srcDir, { recursive: true });
-    await fs.writeFile(path.join(srcDir, `${packageName}.ts`), `
+    await fs.writeFile(
+      path.join(srcDir, `${packageName}.ts`),
+      `
     export default function ${packageName}() {
       console.log("Hello from ${packageName}!");
-    }`);
+    }`
+    );
 
     console.log(`Package "${packageName}" created successfully.`);
   } catch (error: any) {
